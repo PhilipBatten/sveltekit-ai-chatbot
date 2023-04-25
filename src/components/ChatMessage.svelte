e66e7db9e465dc561ad648b24332309aada91425
Philip Batten <philip.batten@efficioconsulting.com>
2023-04-25 09:42:05 +0100

    split out some more components and cleaned up messaege variants

--- src/components/ChatMessage.svelte
+++ src/components/ChatMessage.svelte
@@ -1,32 +1,25 @@
 <script lang="ts">
-	import type { ChatCompletionRequestMessageRoleEnum } from 'openai'
-	export let type: ChatCompletionRequestMessageRoleEnum
-	export let message: string
-    // export let initials: string = 'PB'
+    import MessageAvatar from './MessageAvatar.svelte';
+
+    import MessageTimer from './MessageTimer.svelte';
+
+    import type { ChatCompletionRequestMessageRoleEnum } from 'openai'
+
+    export let type: ChatCompletionRequestMessageRoleEnum
+    export let message: string
 </script>
 
-{#if type === 'assistant'}
-    <div class="flex w-full mt-2 space-x-3 max-w-xs">
-        <div class="flex-shrink-0 relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-200 rounded-full dark:bg-gray-600">
-            <span class="font-medium text-gray-600 dark:text-gray-300"></span>
+<div class="flex w-full mt-2 space-x-3 max-w-xs {type === 'user' ? 'ml-auto justify-end' : ''}">
+    {#if type === 'assistant'}
+        <MessageAvatar />
+    {/if}
+    <div>
+        <div class="{type === 'user' ? 'bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg' : 'bg-gray-300 p-3 rounded-r-lg rounded-bl-lg'}">
+            <p class="text-sm">{message}</p>
         </div>
-        <div>
-            <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
-                <p class="text-sm">{message}</p>
-            </div>
-            <!-- <span class="text-xs text-gray-500 leading-none">2 min ago</span> -->
-        </div>
+        <MessageTimer />
     </div>
-{:else}
-    <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-        <div>
-            <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-                <p class="text-sm">{message}</p>
-            </div>
-            <!-- <span class="text-xs text-gray-500 leading-none">2 min ago</span> -->
-        </div>
-        <div class="flex-shrink-0 relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-200 rounded-full dark:bg-gray-600">
-            <span class="font-medium text-gray-600 dark:text-gray-300"></span>
-        </div>
-    </div>
-{/if}
+    {#if type === 'user'}
+        <MessageAvatar />
+    {/if}
+</div>
---
+++ src/components/MessageAvatar.svelte
@@ -0,0 +1,4 @@
+
+<div class="flex-shrink-0 relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-200 rounded-full dark:bg-gray-600">
+    <span class="font-medium text-gray-600 dark:text-gray-300"></span>
+</div>
